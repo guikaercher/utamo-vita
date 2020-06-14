08a25b95ad901d4e30d16aed6aac12b1cb27ddc1
Guilherme Kaercher <guilherme.kaercher@gmail.com>
2020-06-14 13:49:25 -0300

    adding bat for npm i

---
+++ install-app.bat
@@ -0,0 +1,9 @@
+REM set urlJava="https://javadl.oracle.com/webapps/download/AutoDL?BundleId=242060_3d5a2bb8f8d4428bbe94aed7ec7ae784"
+REM set urlNode="https://nodejs.org/dist/v12.18.0/node-v12.18.0-x64.msi"
+REM start iexplore.exe %url%
+REM start chrome %urlJava%
+REM start chrome %urlNode%
+REM start firefox.exe %url%
+REM start microsoft-edge:%url%
+
+npm i
